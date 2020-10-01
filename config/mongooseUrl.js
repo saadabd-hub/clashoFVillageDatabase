c60fca6dbe0e8792d6c4d23e1f68d9187e5ea2ad
saad-hub99 <62691874+saad-hub99@users.noreply.github.com>
2020-10-01 14:45:47 +0700

    add mongoose configuration

---
+++ config/mongooseUrl.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://localhost/ClashofVillage', {useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    db.on('error', (err) => console.log(err));
+    db.once('open', () => console.log('Database is connection'));
+}
